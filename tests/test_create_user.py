066baecd48c4f4f1aa60dcdcfbbf262c36eac8f2
Viktor Lata <vikost@yandex.ru>
2026-04-20 15:56:45 +0500

    Wrap tests methods to classes

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -6,41 +6,41 @@ from data.urls import BASE_URL, REGISTER
 from data.expected_data import *
 from data.payload_data import REQUIRED_USER_FIELDS
 
+@allure.suite("Тесты создания пользователя")
+class TestCreateUser:
+    @allure.title("Создание уникального пользователя")
+    def test_create_unique_user(self):
+        user = generate_user()
+        response = create_user(user)
+        
+        token = response.json()["accessToken"]
 
-@allure.title("Создание уникального пользователя")
-def test_create_unique_user():
-    user = generate_user()
-    response = create_user(user)
-    
-    token = response.json()["accessToken"]
+        assert response.json()["success"] is True
+        delete_user(token)
 
-    assert response.json()["success"] is True
-    delete_user(token)
+    @allure.title("Создание уже существующего пользователя")
+    def test_create_existing_user(self):
+        user = generate_user()
+        first_response = create_user(user)
+        token = first_response.json()["accessToken"]
 
-@allure.title("Создание уже существующего пользователя")
-def test_create_existing_user():
-    user = generate_user()
-    first_response = create_user(user)
-    token = first_response.json()["accessToken"]
+        response = create_user(user)
+        
+        assert response.status_code == ExpectedResponceCodes.CREATE_DUPLICATE_USER
+        assert response.json()["success"] is False
+        assert response.json()["message"] == ExpectedResponces.CREATE_DUPLICATE_USER
+        delete_user(token)
 
-    response = create_user(user)
-    
-    assert response.status_code == ExpectedResponceCodes.CREATE_DUPLICATE_USER
-    assert response.json()["success"] is False
-    assert response.json()["message"] == ExpectedResponces.CREATE_DUPLICATE_USER
-    delete_user(token)
+    @pytest.mark.parametrize("payload", REQUIRED_USER_FIELDS)
+    @allure.title("Создание пользователя без обязательного поля")
+    def test_create_user_without_required_field(self,payload):
+        user = generate_user()
+        del user[payload]
 
-@pytest.mark.parametrize("payload", REQUIRED_USER_FIELDS)
-@allure.title("Создание пользователя без обязательного поля")
-def test_create_user_without_required_field(payload):
-    user = generate_user()
-    del user[payload]
-
-    response = requests.post(
-        BASE_URL + REGISTER,
-        json=user
-    )
-    assert response.status_code == ExpectedResponceCodes.CREATE_USER_WITHOUT_ANY_PARAM
-    assert response.json()["success"] is False
-    assert response.json()["message"] == ExpectedResponces.CREATE_USER_WITHOUT_ANY_PARAM
-    
+        response = requests.post(
+            BASE_URL + REGISTER,
+            json=user
+        )
+        assert response.status_code == ExpectedResponceCodes.CREATE_USER_WITHOUT_ANY_PARAM
+        assert response.json()["success"] is False
+        assert response.json()["message"] == ExpectedResponces.CREATE_USER_WITHOUT_ANY_PARAM   
--- tests/test_orders.py
+++ tests/test_orders.py
@@ -5,47 +5,49 @@ from data.urls import BASE_URL, ORDERS
 from data.payload_data import VALID_INGREDIENTS, INVALID_INGREDIENT
 from data.expected_data import *
 
-@pytest.mark.parametrize("case_name,ingredients",VALID_INGREDIENTS)
-def test_create_order_authorized(new_user,case_name,ingredients):
-    allure.dynamic.title(f"Создание заказа с авторизацией: {case_name}")
-    _, token = new_user
-    response = requests.post(
-        BASE_URL + ORDERS,
-        headers={"Authorization": token},
-        json={"ingredients":ingredients}
-    )
+@allure.suite("Тесты создания заказа")
+class TestOrder:
+    @pytest.mark.parametrize("case_name,ingredients",VALID_INGREDIENTS)
+    def test_create_order_authorized(self,new_user,case_name,ingredients):
+        allure.dynamic.title(f"Создание заказа с авторизацией: {case_name}")
+        _, token = new_user
+        response = requests.post(
+            BASE_URL + ORDERS,
+            headers={"Authorization": token},
+            json={"ingredients":ingredients}
+        )
 
-    assert response.status_code == 200
-    assert response.json()["success"] is True
+        assert response.status_code == 200
+        assert response.json()["success"] is True
 
-@pytest.mark.parametrize("case_name,ingredients",VALID_INGREDIENTS)
-def test_create_order_without_auth(case_name,ingredients):
-    allure.dynamic.title(f"Создание заказа без авторизации: {case_name} ")
-    response = requests.post(
-        BASE_URL + ORDERS,
-        json={"ingredients": ingredients}
-    )
+    @pytest.mark.parametrize("case_name,ingredients",VALID_INGREDIENTS)
+    def test_create_order_without_auth(self,case_name,ingredients):
+        allure.dynamic.title(f"Создание заказа без авторизации: {case_name} ")
+        response = requests.post(
+            BASE_URL + ORDERS,
+            json={"ingredients": ingredients}
+        )
 
-    assert response.status_code == ExpectedResponceCodes.CREATE_ORDER_UNAUTORIZED_USER
-
-
-@allure.title("Создание заказа без ингредиентов")
-def test_create_order_without_ingredients():
-    response = requests.post(
-        BASE_URL + ORDERS,
-        json={"ingredients": []}
-    )
-
-    assert response.status_code == ExpectedResponceCodes.CREATE_ORDER_EMPTY_INGREDIENTS
-    assert response.json()["success"] is False
-    assert response.json()["message"] == ExpectedResponces.CREATE_ORDER_EMPTY_INGREDIENTS
+        assert response.status_code == ExpectedResponceCodes.CREATE_ORDER_UNAUTORIZED_USER
 
 
-@allure.title("Создание заказа с неверным хешем ингредиентов")
-def test_create_order_invalid_hash():
-    response = requests.post(
-        BASE_URL + ORDERS,
-        json={"ingredients": [INVALID_INGREDIENT]}
-    )
+    @allure.title("Создание заказа без ингредиентов")
+    def test_create_order_without_ingredients(self):
+        response = requests.post(
+            BASE_URL + ORDERS,
+            json={"ingredients": []}
+        )
 
-    assert response.status_code == ExpectedResponceCodes.CREATE_ORDER_INVALID_INGREDIENT_HASH
+        assert response.status_code == ExpectedResponceCodes.CREATE_ORDER_EMPTY_INGREDIENTS
+        assert response.json()["success"] is False
+        assert response.json()["message"] == ExpectedResponces.CREATE_ORDER_EMPTY_INGREDIENTS
+
+
+    @allure.title("Создание заказа с неверным хешем ингредиентов")
+    def test_create_order_invalid_hash(self):
+        response = requests.post(
+            BASE_URL + ORDERS,
+            json={"ingredients": [INVALID_INGREDIENT]}
+        )
+
+        assert response.status_code == ExpectedResponceCodes.CREATE_ORDER_INVALID_INGREDIENT_HASH
